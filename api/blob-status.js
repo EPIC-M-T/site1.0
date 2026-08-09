ccbc56761102cfbb49c461932296ff329ac789d9
LuckyMittenGaming <wsopamatt@gmail.com>
2026-08-08 21:14:44 -0600

    Remove temporary Blob capability probe

--- api/blob-status.js
+++
@@ -1,6 +0,0 @@
-export default function handler(request, response) {
-  response.status(200).json({
-    hasBlobToken: Boolean(process.env.BLOB_READ_WRITE_TOKEN),
-    hasVercelBlobToken: Boolean(process.env.VERCEL_BLOB_READ_WRITE_TOKEN)
-  });
-}
